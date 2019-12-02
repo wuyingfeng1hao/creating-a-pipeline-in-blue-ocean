a6622fde9013c92801a3e86e9a870123350bec27
WANG Hongxing (CI/DAP41.3) <Hongxing.WANG2@cn.bosch.com>
2019-12-02 01:25:04 -0500

    updated npm proxy

---
+++ npm_install.sh
@@ -0,0 +1,3 @@
+npm config set proxy http://won4szh:494560711_SUZHOu@rb-proxy-apac.bosch.com:8080
+npm confit set https-proxy http://won4szh:494560711_SUZHOu@rb-proxy-apac.bosch.com:8080
+npm install
